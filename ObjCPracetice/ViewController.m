eb0e58022048e015ff804a77467189f1a783dbbd
thanhan123 <thanhan.uit@gmail.com>
2017-04-06 23:30:03 +0700

    init

--- ObjCPracetice/ViewController.m
+++ ObjCPracetice/ViewController.m
@@ -16,13 +16,89 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+//    [self solution];
 }
 
+NSMutableArray * solutionIncreaseCounter(int N, NSMutableArray *A) { // 100% correct and 40% performance
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    for (NSInteger i = 0; i < N; i++) {
+        [result addObject:@(0)];
+    }
+    
+    NSInteger maxValue = 0;
+    for (NSInteger i = 0; i < A.count; i++) {
+        if ([A[i] integerValue] <= N) {
+            NSInteger modifyIndex = [A[i] integerValue] - 1;
+            result[modifyIndex] = @([result[modifyIndex] integerValue] + 1);
+            maxValue = maxValue < [result[modifyIndex] integerValue] ? [result[modifyIndex] integerValue] : maxValue;
+        } else if ([A[i] integerValue] == N + 1){
+            for(NSInteger j = 0; j < N; j++){
+                result[j] = @(maxValue);
+            }
+        }
+    }
+    
+    return result;
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// ---------------------------
+
+-(NSInteger)numberOfPalindromicSlices:(NSString*)string{ // 100% correct and 11% performance
+    if (string.length < 2) {
+        return 0;
+    }
+    
+    NSInteger numberOfPalindromicSlices = 0;
+    
+    for (NSInteger i = 0; i < string.length - 1; i++) {
+        for (NSInteger j = i + 1; j < string.length; j++) {
+            NSString *subString = [string substringWithRange:NSMakeRange(i, j - i + 1)];
+            if (isPalindrome(subString)) {
+                numberOfPalindromicSlices++;
+            }
+        }
+    }
+    
+    if (numberOfPalindromicSlices > 100000000) {
+        return -1;
+    }
+    
+    return numberOfPalindromicSlices;
+}
+
+BOOL isPalindrome(NSString *string) {
+    NSInteger length = [string length];
+    for(NSInteger i=0; i<length/2; i++){
+        if ([string characterAtIndex:i] != [string characterAtIndex:(length - 1 - i)]){
+            return NO;
+        }
+    }
+    return YES;
+}
+
+// ------------------
+
+-(NSInteger)solutionPairsOfIntersectingDiscs:(NSArray*)numberArray{ // 100% correct and 25% performance
+    NSInteger pairsOfIntersectingDiscs = 0;
+
+    NSInteger i = 0;
+    while (i < numberArray.count) {
+        NSInteger maxValueOfI = i + [numberArray[i] integerValue];
+        for (NSInteger j = i + 1; j < numberArray.count; j++) {
+            NSInteger minValueOfJ = j - [numberArray[j] integerValue];
+            if (maxValueOfI >= minValueOfJ) {
+                pairsOfIntersectingDiscs ++;
+            }
+        }
+        i++;
+    }
+
+    if (pairsOfIntersectingDiscs > 10000000) {
+        return -1;
+    }
+    
+    return pairsOfIntersectingDiscs;
 }
 
 
---
+++ ObjCPraceticeTests/ObjCPraceticeTests.m
@@ -0,0 +1,73 @@
+//
+//  ObjCPraceticeTests.m
+//  ObjCPraceticeTests
+//
+//  Created by Dinh Thanh An on 4/6/17.
+//  Copyright © 2017 Dinh Thanh An. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <XCTest/XCTest.h>
+
+@interface ObjCPraceticeTests : XCTestCase
+@property (nonatomic) ViewController *vcToTest;
+@end
+
+@interface ViewController (Test)
+-(NSInteger)solutionPairsOfIntersectingDiscs:(NSArray*)numberArray;
+-(NSInteger)numberOfPalindromicSlices:(NSString*)string;
+NSMutableArray * solutionIncreaseCounter(int N, NSMutableArray *A);
+@end
+
+@implementation ObjCPraceticeTests
+
+- (void)setUp {
+    [super setUp];
+    _vcToTest = [[ViewController alloc] init];
+}
+
+- (void)testSolution{
+    NSArray *numberArray = @[@(1), @(5), @(2), @(1), @(4), @(0)];
+    NSInteger result = [_vcToTest solutionPairsOfIntersectingDiscs:numberArray];
+    NSInteger expectedResult = 11;
+    XCTAssertEqual(expectedResult, result, @"test is not passed");
+}
+
+- (void)testSolutionNumberOfPalindromicSlices{
+    NSString *testString = @"baababa";
+    NSInteger result = [_vcToTest numberOfPalindromicSlices:testString];
+    NSInteger expectedResult = 6;
+    XCTAssertEqual(expectedResult, result, @"testSolutionNumberOfPalindromicSlices is not passed");
+}
+
+- (void)testSolutionIncreaseCounter{
+    NSMutableArray *array = [[NSMutableArray alloc] initWithArray:@[@(3),@(4),@(4),@(6),@(1),@(4),@(4)]];
+    NSMutableArray *result = solutionIncreaseCounter(5, array);
+    NSMutableArray *expectedResult = [[NSMutableArray alloc] initWithArray:@[@(3),@(2),@(2),@(4),@(2)]];
+    BOOL isEqual = YES;
+    for (NSInteger i = 0 ; i < 5; i++) {
+        if ([result[i] integerValue] != [expectedResult[i] integerValue]) {
+            isEqual = NO;
+        }
+    }
+    XCTAssertEqual(isEqual, YES, @"testSolutionNumberOfPalindromicSlices is not passed");
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
